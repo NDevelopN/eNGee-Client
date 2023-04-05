9b28153005ba403edff82aebe9f998d2340d1cce
Nathan <NdeveloperN@gmail.com>
2023-04-05 08:35:11 +0100

    Add Lobby page to test component usage

--- engee-client/pages/index.js
+++ engee-client/pages/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState } from 'react';
 
-import Layout, { siteTitle } from '../components/layout'
-import Date from '../components/date'
+import Layout, { siteTitle, updateName } from '../components/layout'
+import {SingleTextForm} from '../components/inputForms'
+import { PlayerList } from '../components/listView';
+import { BlockText } from '../components/outputTexts';
 
 import styles from '../styles/Home.module.css'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home({}) {
+
     return (
         <Layout home>
         <Head>
@@ -17,9 +21,10 @@ export default function Home({}) {
             </title>
         </Head>
 
-        <section className={utilStyles.headingMd}>
-            <p>This is the sample text</p>
-        </section>
+        <main className={styles.main}> 
+            <Link href="/lobby">Go to the Only Lobby</Link>
+        
+        </main>
         
         </Layout>
     );
