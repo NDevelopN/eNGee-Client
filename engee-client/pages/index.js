07e245ae935c7d7e207e3d3ebbd149a513d5cb03
Nathan <NdeveloperN@gmail.com>
2023-04-05 12:28:50 +0100

    Remove unneeded imports from index

--- engee-client/pages/index.js
+++ engee-client/pages/index.js
@@ -1,16 +1,12 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useState } from 'react';
 
 import Layout, { siteTitle, updateName } from '../components/layout'
-import {SingleTextForm} from '../components/inputForms'
-import { PlayerList } from '../components/listView';
-import { BlockText } from '../components/outputTexts';
 
 import styles from '../styles/Home.module.css'
-import utilStyles from '../styles/utils.module.css'
 
 export default function Home({}) {
+
 
     return (
         <Layout home>
@@ -23,7 +19,6 @@ export default function Home({}) {
 
         <main className={styles.main}> 
             <Link href="/lobby">Go to the Only Lobby</Link>
-        
         </main>
         
         </Layout>
