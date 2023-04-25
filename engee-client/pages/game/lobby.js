4ca16d563c54a93fd54f3582e5a32565827d0244
Nathan <NdeveloperN@gmail.com>
2023-04-25 09:27:42 +0100

    Add Leader operations to ingame

--- engee-client/pages/game/lobby.js
+++ engee-client/pages/game/lobby.js
@@ -1,50 +1,13 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 
 import { PlayerList } from '@/components/listView';
 import { POST }  from '@/lib/networkFunctions';
 
-export default function Lobby({pid, gid, leave, changeStatus}) {
+export default function Lobby({pid, gid, leave, plrList}) {
 
     //TODO: transition into gameplay
-    let [plrList, setPlrList] = useState([]);
     let [ready, setReady] = useState(false);
     
-    useEffect(() => {
-        getPlrList();
-        let timer = setInterval(() => getPlrList, 10000);
-    }, []);
-
-    function getPlrList() {
-        let message = {
-            pid: pid,
-            gid: gid,
-        };
-
-        let endpoint = "http://localhost:8080/game/players";
-
-        POST(JSON.stringify(message), endpoint, (e) => {
-            console.log("Got Player List");
-            console.log(e);
-            if (e.players) {
-                setPlrList(e.players);
-            }
-        });
-    }
-
-    function sendLeave() {
-        let message = {
-            pid: pid,
-            gid: gid,
-        };
-
-        let endpoint = "http://localhost:8080/game/leave";
-
-        POST(JSON.stringify(message), endpoint, (e) => {
-            console.log("sent Leave");
-            leave();
-        });
-    }
-
     function changeReady() {
         let message = {
             pid: pid,
@@ -64,7 +27,7 @@ export default function Lobby({pid, gid, leave, changeStatus}) {
         <>
         <PlayerList playerList={plrList}/>
         <span>
-            <button onClick={sendLeave}>Leave</button>
+            <button onClick={leave}>Leave</button>
             <button onClick={changeReady}>{ready ? "Unready" : "Ready"}</button>
         </span>
         </>
