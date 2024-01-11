f1d8806689f52c50e7574e65853416cb011e8ebd
NDevelopN <NDevelopN@gmail.com>
2024-01-11 11:56:40 +0000

    Add User to Home

--- engee-client/src/Home/home.js
+++ engee-client/src/Home/home.js
@@ -1,12 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import User from '../User';
+
+const url = "http://localhost:8090";
 
 function Home() {
     let [Mode, setMode] = useState(0);
+    let [UserInfo, setUserInfo] = useState({'name': "", 'uid': ""});
 
-    switch (Mode) {
-        default:
-            return <h3>Invalid Mode</h3>;
+    useEffect(updateMode, [UserInfo]);
+
+    //TODO define Modes and transitions
+    function updateMode() {
+        if (Mode === 0) {
+            if (UserInfo.uid !== "" && UserInfo.uid !== undefined) {
+                setMode(1);
+            }
+        }
+
+        if (Mode === 1) {
+            if (UserInfo.uid === "" || UserInfo.uid === undefined) {
+                setMode(0);
+            }
+        }
     }
+
+    function ModeScreen() {
+        switch(Mode) {
+            case 0:
+                return <h3>Please set your name.</h3>;
+            case 1:
+                return <h3>Welcome, {UserInfo.name}!</h3>;
+            default:
+                return <h3>Invalid Mode</h3>;
+        }
+    }
+
+    return (
+        <>
+            <User url={url} User={UserInfo} setUser={setUserInfo}/>
+            <ModeScreen/>
+        </>
+    );
 }
 
 export default Home;
