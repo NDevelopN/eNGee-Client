4c3537e161a35629cee82315889c7cab0f829758
NDevelopN <NDevelopN@gmail.com>
2024-01-12 15:58:02 +0000

    Lazy Load Game mode

--- engee-client/src/GameScreen/gameScreen.js
+++ engee-client/src/GameScreen/gameScreen.js
@@ -1,17 +1,37 @@
+import { useState, useEffect, lazy } from 'react';
 import Lobby from '../Lobby';
 
-function GameScreen ({url, userInfo, roomInfo, leave}){
+function GameScreen ({url, userInfo, roomInfo, leave}) {
+
+    let [GameMode, setGameMode] = useState(null);
+
+    const data = {
+        url: url,
+        userInfo: userInfo,
+        roomInfo: roomInfo,
+        leave: leave,
+    }
+
+    useEffect(SelectGameMode, [roomInfo]);
+
+    function SelectGameMode() {
+        switch (roomInfo.gamemode) {
+            case "test":
+                setGameMode(lazy(()=> import('../GameModes/Test')));
+                break;
+
+            default:
+                setGameMode(null);
+                alert("The game mode " + roomInfo.gamemode + " is not supported.");
+                break;
+        }
+    }
+
     return (
     <>
-        {roomInfo.addr !== "" && roomInfo.addr !== undefined
-        ?
-        <h3> This is where the game would be. </h3>
-        : 
-        null
-        }
-
-        <Lobby url={url} userInfo={userInfo} roomInfo={roomInfo} leave={leave}/>
-        </>
+        {GameMode!== null ? <GameMode {...data}/> : null}
+        <Lobby {...data}/>
+    </>
     );
 }
 
