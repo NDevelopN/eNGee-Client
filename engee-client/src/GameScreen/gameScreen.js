be4db9192ae5a6f3aa7cc7f26480e33ba09c85b8
NDevelopN <NDevelopN@gmail.com>
2024-01-17 10:13:56 +0000

    Add interval for getting game address to Game Screen

--- engee-client/src/GameScreen/gameScreen.js
+++ engee-client/src/GameScreen/gameScreen.js
@@ -1,20 +1,61 @@
-import { useState, useEffect, lazy } from 'react';
+import { useState, useEffect, useRef, lazy } from 'react';
 import Lobby from '../Lobby';
+import { httpRequest } from '../net';
+
+
+const addrInterval = 1000;
 
 function GameScreen ({url, userInfo, roomInfo, leave}) {
 
     let [GameMode, setGameMode] = useState(null);
+    let [addr, setAddr] = useState(roomInfo.addr);
 
-    const data = {
-        url: url,
-        userInfo: userInfo,
-        roomInfo: roomInfo,
-        leave: leave,
+    const first = useRef(true);
+    const interval = useRef(null);
+
+
+    useEffect(WaitForAddr, []) 
+    
+    function WaitForAddr() {
+        if (first.current) {
+            first.current = false;
+            return
+        }
+        
+        let endpoint = url + "/rooms/" + roomInfo.rid;
+
+        interval.current = setInterval(GetRoomAddr, addrInterval);
+
+        return (() => {
+            if (interval.current !== null) {
+                clearInterval(interval.current);
+                interval.current = null;
+            }
+        });
+
+
+        function GetRoomAddr() {
+            if (addr !== "" && addr !== undefined) {
+                clearInterval(interval.current);
+                interval.current = null;
+                return;
+            }
+
+            httpRequest("GET", "", endpoint, (rInfo) => {
+                let wsAddress = "ws://" + rInfo.addr + "/games/" + roomInfo.rid + "/players/" + userInfo.uid;
+                setAddr(wsAddress);
+            });
+        }
     }
 
-    useEffect(SelectGameMode, [roomInfo]);
+    useEffect(SelectGameMode, [addr]);
 
     function SelectGameMode() {
+        if (addr === "" || addr === undefined) {
+            setGameMode(null);
+            return;
+        }
+
         switch (roomInfo.gamemode) {
             case "test":
                 setGameMode(lazy(()=> import('../GameModes/Test')));
@@ -29,8 +70,8 @@ function GameScreen ({url, userInfo, roomInfo, leave}) {
 
     return (
     <>
-        {GameMode!== null ? <GameMode {...data}/> : null}
-        <Lobby {...data}/>
+        {GameMode!== null ? <GameMode wsEndpoint={addr} userInfo={userInfo} leave={leave}/> : null}
+        <Lobby url={url} userInfo={userInfo} roomInfo={roomInfo} leave={leave}/>
     </>
     );
 }
